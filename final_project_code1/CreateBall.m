93e4aa74518be2c3e1fe7c51cf266328360f8cec
Yuanqing Hong <yh2866@columbia.edu>
2016-12-03 20:36:21 -0500

    Add plot mode
    add code to plot the whole ball and front-hemisphere and back-hemisphere

--- final_project_code1/CreateBall.m
+++ final_project_code1/CreateBall.m
@@ -1,23 +1,36 @@
 function CreateBall(r,x0,y0,z0,theta)
     global ball_num
+    global plot_mode
     N = 100;
     [x,y,z]=sphere(N); %%The resolution of sphere
     surf(r*x+x0,r*y+y0,r*z+z0);
     k = (r+0.4)/r;
-    y0
     %X=k*r*x+x0;Y=k*r*y+y0;Z=k*r*z+z0;
     X=k*r*x+z0;Y=k*r*y+x0;Z=k*r*z+y0;
     hold on
     %shading interp
    
     %%%
-    kk = (-cos(theta)+1)/2;
-    if ceil(kk*N)<N
-    xxx = X(ceil(kk*N):N,:);
-    yyy = Y(ceil(kk*N):N,:);
-    zzz = Z(ceil(kk*N):N,:);
-    mesh(yyy,zzz,xxx)
-    hold on
+    if plot_mode == 0 || plot_mode == 1
+        kk = (-cos(theta)+1)/2;
+        if ceil(kk*N)<N
+        xxx = X(ceil(kk*N):N,:);
+        yyy = Y(ceil(kk*N):N,:);
+        zzz = Z(ceil(kk*N):N,:);
+        mesh(yyy,zzz,xxx)
+        hold on
+        end
+    end
+    
+    if plot_mode == 2
+        kk = (-cos(theta)+1)/2;
+        if ceil(kk*N)<N
+        xxx = X(ceil(kk*N):N/2,:);
+        yyy = Y(ceil(kk*N):N/2,:);
+        zzz = Z(ceil(kk*N):N/2,:);
+        mesh(yyy,zzz,xxx)
+        hold on
+        end
     end
     %hold on,mesh(X(1:ball_num+1,:),Y(1:ball_num+1,:),Z(1:ball_num+1,:)),colormap(hot)
     %hold on,mesh(X(1:ceil(kk*r),:),Y(1:ceil(kk*r),:),Z(1:ceil(kk*r),:)),colormap(hot) %%Control the mesh
--- final_project_code1/main.m
+++ final_project_code1/main.m
@@ -1,12 +1,17 @@
 clear
 clc
 
-global ball_num %Count the number of trajectory points
+global ball_num  %Count the number of trajectory points
+global plot_mode %Plot mode: 
+                    %0-plot whole sphere; 
+                    %1-plot front-hemisphere;  
+                    %2-plot back-hemisphere;
+plot_mode = 2;
 ball_num = 0;
 DH = [  0,   pi/2,  0.5;...
-        2,      0,    0;...
+        1.8,      0,    0;...
         0,   pi/2,    0;
-        0,  -pi/2,    2;
+        0,  -pi/2,    1.8;
         0,   pi/2,    0;
         0,      0,    0.4];
 
@@ -15,21 +20,32 @@ alpha = DH(:,2);
 d = DH(:,3);
 
 %Parameters of sphere
-% r = 0.6;
-% Ox = 1.5;
-% Oy = 1.5;
-% Oz = 1.5;
 r = 0.5;
 Ox = 0;
 Oy = 1.5;
 Oz = 1.5;
 %view(42,12);
-for theta = 0.1:0.2:pi
-    for phi = 0.1:0.5:2*pi
-        Px = Ox + r*sin(theta)*cos(phi);
-        Pz = Oz + r*sin(theta)*sin(phi);
-        Py = Oy - r*cos(theta);
-        tr = [Px;Py;Pz];
-        testMain(a,alpha,d,tr,theta,phi,r,Ox,Oy,Oz);
+%%%Plot Whole Sphere
+if plot_mode == 0
+    for theta = 0.1:0.2:pi
+        for phi = 0.1:0.5:2*pi
+            Px = Ox + r*sin(theta)*cos(phi);
+            Pz = Oz + r*sin(theta)*sin(phi);
+            Py = Oy - r*cos(theta);
+            tr = [Px;Py;Pz];
+            testMain(a,alpha,d,tr,theta,phi,r,Ox,Oy,Oz);
+        end
+    end
+end
+%%%Plot Front-Hemisphere
+if plot_mode == 1 || plot_mode == 2
+    for theta = 0.1:0.2:pi/2
+        for phi = 0.1:0.5:2*pi
+            Px = Ox + r*sin(theta)*cos(phi);
+            Pz = Oz + r*sin(theta)*sin(phi);
+            Py = Oy - r*cos(theta);
+            tr = [Px;Py;Pz];
+            testMain(a,alpha,d,tr,theta,phi,r,Ox,Oy,Oz);
+        end
     end
 end
